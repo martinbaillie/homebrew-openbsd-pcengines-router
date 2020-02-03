bd3df517165426efd0b4d3fb8de99db5dd4ac305
Martin Baillie <martin@baillie.email>
2020-02-03 19:14:34 +1100

    Allow adserver overrides

--- files/adservers_mkdb.sh
+++ files/adservers_mkdb.sh
@@ -1,10 +1,18 @@
-#!/usr/bin/env bash
+#!/usr/local/bin/bash
 set -euo pipefail
 
-# Populate a new monster DB
+: "${CURL:=/usr/local/bin/curl}"
+
+EXCLUSION_GREP=""
+while read -r permitted; do
+    [ -n "${permitted}" ] && EXCLUSION_GREP="${EXCLUSION_GREP} -e '${permitted}'"
+done < /etc/adservers_permitted.db
+[ -n "$EXCLUSION_GREP" ] && EXCLUSION_GREP="grep -v -i ${EXCLUSION_GREP}"
+
+# Populate a new monster DB.
 unset LANG && \
-    for site in $(curl --fail -s 'https://v.firebog.net/hosts/lists.php?type=tick'); do
-        curl --fail -sL "${site}" || :;
+    for site in $(${CURL} --fail -s 'https://v.firebog.net/hosts/lists.php?type=tick'); do
+        ${CURL} --fail -sL "${site}" || :;
     done | \
     sed -e 's/^127.0.0.1\s*//g; s/^0.0.0.0\s*//g; s/^0\s*//g; s/localhost//g' \
     -e 's/\s*#.*$//' -e '/^\s*$/d' -e $'s/\r//' | \
@@ -14,19 +22,18 @@ unset LANG && \
     sed '/^-/d; /^\./d; /^\s*$/d; /\.\./d' | \
     sort | \
     uniq | \
+    eval "${EXCLUSION_GREP}" | \
     { 
         while read -r site; do 
             echo -e "local-zone: \"${site}\" refuse";
         done
     } > /etc/adservers.db
-# OLD
-#/usr/local/bin/curl -Ls 'https://pgl.yoyo.org/adservers/serverlist.php?hostformat=unbound&showintro=0&startdate%5Bday%5D=&startdate%5Bmonth%5D=&startdate%5Byear%5D=&mimetype=plaintext' -o /etc/adservers.db >/dev/null
 
-# Restart unbound
+# Restart unbound.
 chown _unbound:_unbound /etc/adservers.db
 pkill -9 -u _unbound unbound
 rcctl stop unbound
 rcctl start unbound
 
-# Warm up
-curl -LIs www.google.com > /dev/null || curl -LIs www.google.com > /dev/null 
+# Warm up.
+${CURL} -LIs www.google.com > /dev/null || ${CURL} -LIs www.google.com > /dev/null 
